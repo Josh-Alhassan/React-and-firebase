36eae5a78d98d90c07cab53ec0640a3bb1c25607
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-12 20:22:57 +0000

    click event and event handler

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 
 function App() {
+  const title = 'Learning React is freaking Awesome';
+  const subtitle = 'This is a website for learning react.js';
+
+  const handleClick = (e) => {
+    console.log('You clicked me')
+    console.log('The site description is ', title)
+    console.log(e)
+  }
   return (
     <div className="App">
       <h1>Build web apps with React</h1>
+      <button onClick={handleClick}>Click Me</button>
+      {/* inline event handler */}
+      <button onClick={(e) => {
+        console.log('Inline Event Handler');
+        console.log(e)
+      }} >Another BTN</button>
     </div>
   );
 }
