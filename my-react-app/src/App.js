2aa73ca7c5827e59bbef1e0eb565e70f7bb72ece
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-10-13 18:13:43 +0000

    using the useState hook

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [name, setName] = useState('Joshua');
+
   const title = 'Learning React is freaking Awesome';
   const subtitle = 'This is a website for learning react.js';
 
   const handleClick = (e) => {
-    console.log('You clicked me')
-    console.log('The site description is ', title)
-    console.log(e)
+    setName('Alhassan')
   }
   return (
     <div className="App">
       <h1>Build web apps with React</h1>
-      <button onClick={handleClick}>Click Me</button>
-      {/* inline event handler */}
-      <button onClick={(e) => {
-        console.log('Inline Event Handler');
-        console.log(e)
-      }} >Another BTN</button>
+      <p>My name is {name}</p>
+      <button onClick={handleClick}>Change name</button>
     </div>
   );
 }
